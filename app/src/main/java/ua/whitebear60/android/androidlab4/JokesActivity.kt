3ce37f27ab0c74c10c25163134deaa71ec34369b
Mykhailo <mykhailo.yaremenko@outlook.com>
2025-03-13 10:34:16 +0200

    Initial commit

---
+++ app/src/main/java/ua/whitebear60/android/androidlab4/JokesActivity.kt
@@ -0,0 +1,20 @@
+package ua.whitebear60.android.androidlab4
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class JokesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_jokes)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
