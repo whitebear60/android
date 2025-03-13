39798866217557a34bfd45b5ee2bd886a0a774c8
Mykhailo <mykhailo.yaremenko@outlook.com>
2025-03-13 10:33:50 +0200

    Optimise imports

--- app/src/main/java/ua/whitebear60/android/androidlab4/MainActivity.kt
+++ app/src/main/java/ua/whitebear60/android/androidlab4/MainActivity.kt
@@ -1,22 +1,13 @@
 package ua.whitebear60.android.androidlab4
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.content.ContextCompat
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.google.android.material.color.MaterialColors
 import ua.whitebear60.android.androidlab4.databinding.ActivityMainBinding
 import ua.whitebear60.android.androidlab4.ui.theme.AndroidLab4Theme
 
